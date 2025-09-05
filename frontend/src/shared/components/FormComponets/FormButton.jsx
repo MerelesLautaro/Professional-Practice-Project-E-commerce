ff2c7110c847050cf00bbe6e021045cf4697f4f1
Ezequiel Beuter <ezequielbeuter@gmail.com>
2025-09-05 11:40:14 -0300

    Modulo de Button.css creado e implementado

--- frontend/src/shared/components/FormComponets/FormButton.jsx
+++ frontend/src/shared/components/FormComponets/FormButton.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-const FormButton = ({ label, submit, ...rest })=>{
-    return(
-        <button type={submit ? "submit" : "button"} {...rest}>
-          {label}
-        </button>
-    )
-}
-export default FormButton
+import styles from "./styles/Button.module.css";
+const FormButton = ({ label, submit, ...rest }) => {
+  return (
+    <button style={styles.button} type={submit ? "submit" : "button"} {...rest}>
+      {label}
+    </button>
+  );
+};
+export default FormButton;
