31a5550e7386d9439bf4bb2fb08ee50e739917ce
Ezequiel Beuter <ezequielbeuter@gmail.com>
2025-09-05 15:09:15 -0300

    Modulo de InputField.css creado e implementado/EC-55

--- frontend/src/shared/components/FormComponets/InputField.jsx
+++ frontend/src/shared/components/FormComponets/InputField.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import { useField } from "formik";
 import { FiEye, FiEyeOff } from "react-icons/fi";
+import styles from './styles/InputField.module.css'
 
 const InputField = ({ type, name, label, ...rest }) => {
   const [field, meta] = useField(name);
@@ -11,18 +12,18 @@ const InputField = ({ type, name, label, ...rest }) => {
     type === "password" ? (showPassword ? "text" : "password") : type;
 
   return (
-    <div className="input-field">
+    <div className={styles.inputField}>
       {label && <label htmlFor={name}>{label}</label>}
-      <div className="input-wrapper">
+      <div className={styles.inputWrapper}>
         <input id={name} type={inputType} {...field} {...rest} />
         {type === "password" && (
-          <span className="password-toggle" onClick={togglePassword}>
+          <span className={styles.passwordToggle} onClick={togglePassword}>
             {showPassword ? <FiEyeOff size={20} /> : <FiEye size={20} />}
           </span>
         )}
       </div>
 
-      {meta.touched && meta.error && <div className="error">{meta.error}</div>}
+      {meta.touched && meta.error && <div className={styles.error}>{meta.error}</div>}
     </div>
   );
 };
