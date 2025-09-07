0c3b3007c27a700c6b65113015bbc5697a7347b6
Ezequiel Beuter <ezequielbeuter@gmail.com>
2025-09-07 15:17:01 -0300

    Modulo Container.css creado e implementado/EC-55

--- frontend/src/features/register/Register.jsx
+++ frontend/src/features/register/Register.jsx
@@ -2,6 +2,7 @@ import {Container, DynamicForm } from "../../shared/components";
 import "@fontsource/iceberg";
 import { initialValues, validationSchema } from "./validations";
 import { formElements } from "./config";
+import styles from './registerStyle.module.css'
 
 function Register() {
   const handleSubmit = (values) => {
@@ -10,11 +11,7 @@ function Register() {
 
   return (
       <Container>
-        <div className="forms-title">
-          <h1>Registrarse | </h1>
-          <h2>¿Ya tienes cuenta?</h2>
-          <a href="#">Ingresá aquí</a>
-        </div>
+
         <DynamicForm
           elements={formElements}
           initialValues={initialValues}
